75952e17da1f46ce32ab78f625a33062a8351098
justhands13 <shashank11235@gmail.com>
2020-10-16 11:20:44 +0530

    weather report generator initialised

---
+++ src/main/java/com/example/weather/weather_report_generator/WeatherReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather.weather_report_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReportGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherReportGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weather/weather_report_generator/WeatherReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weather.weather_report_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherReportGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
